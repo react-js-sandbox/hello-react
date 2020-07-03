daf04ffb060ed6c27e3ae6ddc8528c5ac9f5c914
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-03 23:21:30 +0300

    props

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-function Greeting() {
-    return (<h1>Hello react!!!</h1>);
+function Greeting(props) {
+    return (<div><h1>{props.text}, children: {props.children}</h1></div>);
 }
 
 ReactDOM.render(<div>
-    <Greeting/><Greeting/>
+    <Greeting text='Hi there'/><Greeting/>
+    <Greeting text='Aloha!'/>Bro<Greeting/>
 </div>, document.getElementById('root'));
